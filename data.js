5600cb7b9123aee422e648ae354f8efaf96432fc
Jessica Rendón Buitrago <JessicaRendon@Jessicas-MacBook-Pro.local>
2019-11-24 17:26:43 -0500

    images added

---
+++ data.js
@@ -0,0 +1,12 @@
+module.exports = [
+    
+    {
+        type: "COMIC",   
+        name: "ANTHEM - A New Verse",
+        description: "Dark Horse Comics",
+        img: "da",
+        price: 21.95,
+        year: 2017,
+    },
+    
+]
